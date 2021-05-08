d8a12f5ba32a57adf9861dd49a0da759fdcc4148
Joao_Normando <joao.normando@maisunifacisa.com.br>
2021-05-08 17:11:15 -0300

    Env var

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -36,7 +36,7 @@ const BarChart = () => {
             .then(response => {
                 const data = response.data as SaleSucess[];
                 const myLabels = data.map(x => x.sellerName);
-                const mySeries = data.map(x => round(100.00 * x.deals / x.visited, 1));
+                const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1));
 
                 setChartData({
                     labels: {
@@ -60,15 +60,15 @@ const BarChart = () => {
             }
         },
     };
+    
 
     return (
         <Chart
-            options={{ ...options, xaxis: chartData.labels }}
+            options={{ ...options, xaxis: chartData.labels}}
             series={chartData.series}
             type="bar"
             height="240"
         />
-
     );
 }
 
